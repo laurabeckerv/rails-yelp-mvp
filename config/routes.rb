0e1220beb7b26186a12f0638a66dbb9f8be76b3d
Laura Beckervordersandforth <laura.beckerv@gmail.com>
2022-08-24 16:12:19 +0200

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'restaurants/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
