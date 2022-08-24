effd29284eb30277e760c008c498e7bfeb93affc
Laura Beckervordersandforth <laura.beckerv@gmail.com>
2022-08-24 15:56:46 +0200

    all pages are set up

---
+++ app/models/restaurant.rb
@@ -0,0 +1,9 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  # dependent: :destroy -> if a restaurant is deleted, all of its reviews are deleted as well
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: { in: %w(Chinese Italian Japanese French Belgian),
+    message: "%{value} is not a valid category" }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+restaurants = Restaurant.create([{name: "Restaurant 1", address: "1, Main Street", phone_number: "123456789", category: "chinese"},
+                                {name: "Restaurant 2", address: "2, Main Street", phone_number: "123456789", category: "italian"},
+                                {name: "Restaurant 3", address: "3, Main Street", phone_number: "123456789", category: "japanese"},
+                                {name: "Restaurant 4", address: "4, Main Street", phone_number: "123456789", category: "french"},
+                                {name: "Restaurant 5", address: "5, Main Street", phone_number: "123456789", category: "belgian"}])
